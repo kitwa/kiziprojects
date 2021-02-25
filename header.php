ed8cf5eec6d7620d92e90629d91a0fa4a81c2219
kitwa <dominique@lucidocean.com>
2021-02-25 19:15:06 +0200

    Meta Tags to improve the way the post looks when you share the page

--- header.php
+++ header.php
@@ -10,10 +10,10 @@ We have been involved in various projects including developing building, renovat
     <meta name="author" content="kiziprojects">
 
     <!-- OG Meta Tags to improve the way the post looks when you share the page on LinkedIn, Facebook, Google+ -->
-	<meta property="og:site_name" content="" /> <!-- website name -->
-	<meta property="og:site" content="" /> <!-- website link -->
-	<meta property="og:title" content=""/> <!-- title shown in the actual shared post -->
-	<meta property="og:description" content="" /> <!-- description shown in the actual shared post -->
+	<meta property="og:site_name" content="Kiziprojects" /> <!-- website name -->
+	<meta property="og:site" content="www.kiziprojects.com" /> <!-- website link -->
+	<meta property="og:title" content="Kizi Projects"/> <!-- title shown in the actual shared post -->
+	<meta property="og:description" content="Building Renovation Construction Management" /> <!-- description shown in the actual shared post -->
 	<meta property="og:image" content="" /> <!-- image link, make sure it's jpg -->
 	<meta property="og:url" content="" /> <!-- where do you want your post to link to -->
 	<meta property="og:type" content="article" />
